0a9726ac1a088fecd29e54b5e9100f225c7704ac
Neha121995 <jagtapneha1212@gmail.com>
2021-08-07 19:21:39 +0530

    [Neha]Added: Function to create contacts in address book

---
+++ AddressBookCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book_Using_Collection
+{
+    class AddressBookCollection
+    {
+        Contacts person = new Contacts();
+    }
+}
