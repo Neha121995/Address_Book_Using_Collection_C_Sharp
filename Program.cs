3bd9ed8ebee220212e41674e659b8c302facfc62
Neha121995 <jagtapneha1212@gmail.com>
2021-08-07 21:44:53 +0530

    [Neha]Added: Function to search person in a city or state in address book

--- Program.cs
+++ Program.cs
@@ -8,16 +8,18 @@ namespace Address_Book_Using_Collection
         {
             Console.WriteLine("Welcome to Address Book using Collection");
             AddressBookCollection.addAddressBook();
-            Console.WriteLine("\n");
-            AddressBookCollection.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBookCollection.EditContact();
-            Console.WriteLine("\n");
-            AddressBookCollection.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBookCollection.ContactsDisplay();
-            AddressBookCollection.ContactsDisplay();
-            AddressBookCollection.ContactsDisplay();
-            AddressBookCollection.ContactsDisplay();
+            AddressBookCollection.PersonSearch();
+            //Console.WriteLine("\n");
+            //AddressBookCollection.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBookCollection.EditContact();
+            //Console.WriteLine("\n");
+            //AddressBookCollection.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBookCollection.ContactsDisplay();
+            //AddressBookCollection.ContactsDisplay();
+            //AddressBookCollection.ContactsDisplay();
+            //AddressBookCollection.ContactsDisplay();
         }
+    }
 }
