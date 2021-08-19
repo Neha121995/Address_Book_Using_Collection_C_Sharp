cf16969be44315c369d1e05e9f167ea7b7594495
Neha121995 <jagtapneha1212@gmail.com>
2021-08-19 18:53:15 +0530

    [Neha] Added: Function to create new contacts

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿-- Create Database : 
+
+create database addressbook;
+
+-- Use Database : 
+use addressbook;
+
+-- Creating Table :
+create table addressBook
+(
+	Id int identity primary key,
+	FirstName varchar(50),
+	LastName varchar(50),
+	Address varchar(100),
+	City varchar(50),
+	State varchar(50),
+	ZipCode varchar(6),
+	PhoneNumber varchar(11),
+	EmailId varchar(50)
+);
+
+select * from addressBook;
